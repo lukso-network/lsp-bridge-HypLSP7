5bc6899181ddfb3b4384a869be6bf2bb2a367198
Justin <justin@universaleverything.io>
2025-02-21 10:30:24 +0100

    removed whitespace for linter

--- src/ISM/PausableISM.sol
+++ src/ISM/PausableISM.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0;
 // ============ External Imports ============
 import { Pausable } from "@openzeppelin/contracts/security/Pausable.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { IInterchainSecurityModule } from "@hyperlane-xyz/core/contracts/interfaces/IInterchainSecurityModule.sol";           
+import { IInterchainSecurityModule } from "@hyperlane-xyz/core/contracts/interfaces/IInterchainSecurityModule.sol";
 // ============ Internal Imports ============
 import { CircuitBreakerAdapter } from "./CircuitBreakerAdapter.sol";
 
