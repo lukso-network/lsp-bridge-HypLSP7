816722dc43a11e06f7fca239241723a86c819ef6
Justin <justin@universaleverything.io>
2025-06-16 18:26:13 +0200

    copy/pasted override of TokenRouter functions

--- src/pausable/HypNativePausable.sol
+++ src/pausable/HypNativePausable.sol
@@ -3,8 +3,28 @@ pragma solidity >=0.8.19;
 
 // modules
 import { HypNative } from "@hyperlane-xyz/core/contracts/token/HypNative.sol";
-import { CircuitBreaker } from "./CircuitBreaker.sol";
+import { Freezable } from "./Freezable.sol";
 
-contract HypNativePausable is HypNative, CircuitBreaker {
+contract HypNativePausable is HypNative, Freezable {
     constructor(address _mailbox) HypNative(_mailbox) { }
+
+    function _transferRemote(
+        uint32 _destination,
+        bytes32 _recipient,
+        uint256 _amountOrId,
+        uint256 _value,
+        bytes memory _hookMetadata,
+        address _hook
+    ) internal virtual override whenNotPaused() returns (bytes32 messageId) {
+        return super._transferRemote(_destination, _recipient, _amountOrId, _value, _hookMetadata, _hook);
+    }
+
+    function _transferTo(
+        address _recipient,
+        uint256 _amount,
+        bytes calldata  _metadata
+    ) internal virtual override whenNotPaused() {
+        HypNative._transferTo(_recipient, _amount, _metadata);
+    }
+
 }
