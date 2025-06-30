70b3c1e83aae186f6671fc48e1b7a6b45bb5897a
CJ42 <cavallerajean@gmail.com>
2025-06-30 11:46:25 +0200

    refactor: rename `initialize(...)` parameter to avoid state variables shadowing

--- src/HypLSP7.sol
+++ src/HypLSP7.sol
@@ -26,9 +26,9 @@ contract HypLSP7 is LSP7DigitalAssetInitAbstract, FungibleTokenRouter {
 
     /**
      * @notice Initializes the Hyperlane router, LSP7 metadata, and mints initial supply to deployer.
-     * @param _totalSupply The initial supply of the token.
-     * @param _name The name of the token.
-     * @param _symbol The symbol of the token.
+     * @param initialSupply The initial supply of the token.
+     * @param tokenName The name of the token.
+     * @param tokenSymbol The symbol of the token.
      *
      * @dev The `LSP4TokenType` is hardcoded to type `Token` (= `0`) as all ERC20 tokens are of token type 0.
      * This aims to keep the number of parameters consistent between hyperc20 and hypLSP7, so that the code of off-chain
@@ -38,12 +38,12 @@ contract HypLSP7 is LSP7DigitalAssetInitAbstract, FungibleTokenRouter {
      * will be triggered, even if the `_totalSupply` parameter passed is 0.
      */
     function initialize(
-        uint256 _totalSupply,
-        string memory _name,
-        string memory _symbol,
-        address _hook,
-        address _interchainSecurityModule,
-        address _owner,
+        uint256 initialSupply,
+        string memory tokenName,
+        string memory tokenSymbol,
+        address defaultHook,
+        address defaultInterchainSecurityModule,
+        address contractOwner,
         bytes32[] memory dataKeys,
         bytes[] memory dataValues
     )
@@ -52,9 +52,9 @@ contract HypLSP7 is LSP7DigitalAssetInitAbstract, FungibleTokenRouter {
     {
         // Initialize LSP7 metadata
         LSP7DigitalAssetInitAbstract._initialize({
-            name_: _name,
-            symbol_: _symbol,
-            newOwner_: _owner,
+            name_: tokenName,
+            symbol_: tokenSymbol,
+            newOwner_: contractOwner,
             lsp4TokenType_: _LSP4_TOKEN_TYPE_TOKEN,
             isNonDivisible_: false // isNonDivisible set to `false` as not used anyway since decimals() is overriden
          });
@@ -65,13 +65,15 @@ contract HypLSP7 is LSP7DigitalAssetInitAbstract, FungibleTokenRouter {
         }
 
         // mints initial supply to deployer
-        LSP7DigitalAssetInitAbstract._mint({ to: msg.sender, amount: _totalSupply, force: true, data: "" });
+        LSP7DigitalAssetInitAbstract._mint({ to: msg.sender, amount: initialSupply, force: true, data: "" });
 
         // Initializes the warp route
-        _MailboxClient_initialize(_hook, _interchainSecurityModule, _owner);
+        _MailboxClient_initialize(defaultHook, defaultInterchainSecurityModule, contractOwner);
     }
 
-    function decimals() public view virtual override returns (uint8) {
+    /// @dev Override the default `decimals()` function that relies on `isNonDivisible_` parameter
+    /// to use the `_decimals` immutable variable set above instead.
+    function decimals() public view virtual override(LSP7DigitalAssetInitAbstract) returns (uint8) {
         return _decimals;
     }
 
