ea6d603053bfddf834dc49a5f7e1a93c7518275a
CJ42 <cavallerajean@gmail.com>
2025-06-30 16:14:08 +0200

    refactor: clear solidity mixed cases variables from slither output

--- src/HypLSP8Collateral.sol
+++ src/HypLSP8Collateral.sol
@@ -53,46 +53,46 @@ contract HypLSP8Collateral is TokenRouter {
         _MailboxClient_initialize(defaultHook, defaultInterchainSecurityModule, contractOwner);
     }
 
-    function ownerOf(uint256 _tokenId) external view returns (address) {
-        return wrappedToken.tokenOwnerOf(bytes32(_tokenId));
+    function ownerOf(uint256 tokenId) external view returns (address) {
+        return wrappedToken.tokenOwnerOf(bytes32(tokenId));
     }
 
     /**
-     * @dev Returns the balance of `_account` for `wrappedToken`.
+     * @dev Returns the balance of `account` for `wrappedToken`.
      * @inheritdoc TokenRouter
      */
-    function balanceOf(address _account) external view override returns (uint256) {
-        return wrappedToken.balanceOf(_account);
+    function balanceOf(address account) external view override returns (uint256) {
+        return wrappedToken.balanceOf(account);
     }
 
     /**
-     * @dev Transfers `_tokenId` of `wrappedToken` from `msg.sender` to this contract.
+     * @dev Transfers `tokenId` of `wrappedToken` from `msg.sender` to this contract.
      * Note that this function will also trigger a callback to the `universalReceiver(...)` function
      * on the `msg.sender` if it is a contract that supports + implements the LSP1 standard.
      *
      * @inheritdoc TokenRouter
      */
-    function _transferFromSender(uint256 _tokenId) internal virtual override returns (bytes memory) {
-        wrappedToken.transfer(msg.sender, address(this), bytes32(_tokenId), true, "");
+    function _transferFromSender(uint256 tokenId) internal virtual override returns (bytes memory) {
+        wrappedToken.transfer(msg.sender, address(this), bytes32(tokenId), true, "");
         return bytes(""); // no metadata
     }
 
     /**
-     * @dev Transfers `_tokenId` of `wrappedToken` from this contract to `_recipient`.
+     * @dev Transfers `tokenId` of `wrappedToken` from this contract to `recipient`.
      * Note that this function will also trigger a callback to the `universalReceiver(...)` function
-     * on the `_recipient` if it is a contract that supports + implements the LSP1 standard.
+     * on the `recipient` if it is a contract that supports + implements the LSP1 standard.
      *
      * @inheritdoc TokenRouter
      */
     function _transferTo(
-        address _recipient,
-        uint256 _tokenId,
+        address recipient,
+        uint256 tokenId,
         bytes calldata // no metadata
     )
         internal
         virtual
         override
     {
-        wrappedToken.transfer(address(this), _recipient, bytes32(_tokenId), true, "");
+        wrappedToken.transfer(address(this), recipient, bytes32(tokenId), true, "");
     }
 }
