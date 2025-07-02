58cd203227c4ca46b8fc2b0008359687e5404080
CJ42 <cavallerajean@gmail.com>
2025-07-02 17:57:21 +0200

    chore: update software license in Solidity files

--- test/helpers/LSP8Mock.sol
+++ test/helpers/LSP8Mock.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import { LSP8IdentifiableDigitalAsset } from "@lukso/lsp8-contracts/contracts/LSP8IdentifiableDigitalAsset.sol";
