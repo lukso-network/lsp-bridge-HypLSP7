f1c2d362508b70934acf17391c8663e9c163d4f5
CJ42 <cavallerajean@gmail.com>
2025-11-19 14:58:56 +0000

    chore: update software license to Apache-2.0

--- test/helpers/LSP7Mock.sol
+++ test/helpers/LSP7Mock.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
 import { LSP7DigitalAsset } from "@lukso/lsp7-contracts/contracts/LSP7DigitalAsset.sol";
--- test/helpers/LSP8Mock.sol
+++ test/helpers/LSP8Mock.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
 import { LSP8IdentifiableDigitalAsset } from "@lukso/lsp8-contracts/contracts/LSP8IdentifiableDigitalAsset.sol";
